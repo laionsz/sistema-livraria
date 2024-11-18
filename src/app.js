830f748adb2c5d8fd3efe6a768f1855d2eeaac55
Laion <laion.souza0507@gmail.com>
2024-11-18 15:48:27 -0300

    Adicionado models, config, database connect e processo dotenv

--- src/app.js
+++ src/app.js
@@ -1,35 +1,32 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso");
+})
 
 const app = express();
 app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    });
-};
 
 app.get("/", (req, res) => {
     res.status(200).send("Sistema de livraria");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros)
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros)
 });
 
 app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id)
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
 })
 
 app.post("/livros", (req, res) => {
@@ -37,4 +34,17 @@ app.post("/livros", (req, res) => {
     res.status(201).send("Livro cadastrado!");
 });
 
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("livro removido com sucesso");
+});
+
 export default app;
+
